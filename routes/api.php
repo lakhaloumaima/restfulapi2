58a9403019ca51ca1222cf7c6226bd96044b23a1
oumaima <lakhalouma6@gmail.com>
2022-02-05 23:12:14 +0100

    test2

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register' , 'AuthController@register') ;
+Route::post('/login' , 'AuthController@login') ;
+
+Route::middleware('auth:api')->group( function (){
+    Route::resource('posts', 'PostController');
+    Route::get('posts/user/{id}', 'PostController@userPosts');
+});
+
+
